7a8f3330f4a3110199a4e1197266b3b27634d0eb
noir4y <210010039+noir4y@users.noreply.github.com>
2025-09-26 22:00:19 +0900

    Add 0001-two-sum.php

---
+++ php/0001-two-sum.php
@@ -0,0 +1,26 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param int[] $nums
+     * @param int $target
+     * @return int[]
+     */
+    function twoSum($nums, $target)
+    {
+        $map = [];
+
+        foreach ($nums as $index => $num) {
+            $complement = $target - $num;
+
+            if (isset($map[$complement])) {
+                return [$map[$complement], $index];
+            }
+
+            $map[$num] = $index;
+        }
+
+        return [];
+    }
+}
